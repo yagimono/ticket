18040a4b47c6017566234b066a1dae0e02834276
八木健太 <yagi@monosense.co.jp>
2019-04-23 15:27:45 +0900

    ログインしていない場合はログインページへ

---
+++ src/router/guards.js
@@ -0,0 +1,18 @@
+import store from '../store'
+
+export const authorizeToken = (to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        // マッチしたルートにおいて、メタフィールドに`requiresAuth`が付与されている場合は
+        // ログインした際に付与される認証とーくんがあるかどうかチェックする
+        // 注意:
+        // このアプリケーションでは簡略化のため`auth.token`があるかどうかのみで
+        // 本来ならば付与された認証トークンをバックエンドのAPI経由などで検証すべき
+        if (!store.state.auth.auth || !store.state.auth.auth.token) {
+            next({ path: '/login' })
+        } else {
+            next()
+        }
+    } else {
+        next()
+    }
+}
